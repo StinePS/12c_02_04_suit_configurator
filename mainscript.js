eae8dcb32ca69206a5b1fd40d53bd56fb89c0bfc
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2021-10-14 10:21:08 +0200

    beginning of configurator and menu

--- mainscript.js
+++ mainscript.js
@@ -1,3 +1,34 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", getData);
+
+// The model of all features
+const features = {
+  fabric: false,
+  lapel: false,
+  pocketsquare: true,
+  pocketflaps: false,
+  buttons: false,
+};
+
+async function getData() {
+  console.log("getData");
+  let response = await fetch("suit1.svg");
+  let svgData = await response.text();
+  document.querySelector("#product-preview").innerHTML = svgData;
+  init();
+}
+
+function init() {
+  // Register clicks
+  document.querySelectorAll(".attribute").forEach((attribute) => attribute.addEventListener("click", displayFlyout));
+}
+
+function displayFlyout(event) {
+  const target = event.currentTarget;
+  const feature = target.dataset.feature;
+  console.log("displayFlyout", target.dataset.feature);
+  document.querySelector(".flyout-panel").classList.remove("flyout-panel-display");
+  document.querySelector(`#flyout-menu  [data-feature=${feature}]`).classList.add("flyout-panel-display");
+  init();
+}
